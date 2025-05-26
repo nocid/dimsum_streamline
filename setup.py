772824edf5582a0a67162f7541a7dfac80e6d11c
Romain Pastre <64286255+nocid@users.noreply.github.com>
2025-05-26 17:44:49 +0200

    Update setup.py
    modify comments

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dimsum_streamline',  # **Required**:  Your package name (what users will use to install, e.g., `pip install my_package`)
-    version='0.1.0',   # **Required**:  Start with a version number
-    packages=find_packages(),  # **Required**:  Automatically find all packages and subpackages
+    name='dimsum_streamline',  
+    version='0.1.0',   
+    packages=find_packages(),  
     install_requires=[os, sys, pandas, numpy, matplotlib, subprocess,argparse,time,getpass
-        # List any dependencies your package needs here, e.g., 'requests >= 2.20'
+        
     ],
-    python_requires='>=3.6',  # Specify minimum Python version if needed
-    description='Automatize the process of running dimsum from local to remote',  # Short, informative description
-    long_description=open('README.md').read() if os.path.exists('README.md') else '', # Optional: Long description from README
-    long_description_content_type='text/markdown', # If you're using Markdown for README
-    author='Romain Pastre',        # Your name
-    author_email='rpastre@ibec.barcelona.eu', # Your email
-    url='https://github.com/yourusername/my_package', # Link to your GitHub repository (once created)
-    license='MIT',          # Choose a license (e.g., MIT, Apache 2.0, GPL)
+    python_requires='>=3.6',  
+    description='Automatize the process of running dimsum from local to remote',  
+    long_description=open('README.md').read() if os.path.exists('README.md') else '', 
+    long_description_content_type='text/markdown', 
+    author='Romain Pastre',     
+    author_email='rpastre@ibec.barcelona.eu',
+    url='https://github.com/nocid/dimsum_streamline', 
+    license='MIT',        
     classifiers=[
-        # Optional: Classifiers help users find your package on PyPI
-        'Development Status :: 3 - Alpha', # Or '4 - Beta', '5 - Production/Stable'
+       
+        'Development Status :: 3 - Alpha', 
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
